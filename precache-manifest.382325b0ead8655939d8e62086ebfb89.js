d13028cd7ffe309cbae141e9016a550015e3bc6c
Bryan Wong <bryanwongzhicheng@gmail.com>
2020-06-10 03:02:35 -0400

    Updates

---
+++ precache-manifest.382325b0ead8655939d8e62086ebfb89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e074e7b96fe9c12baa69c0d57ce71d7",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "ff55a1afe5c7dac5c2cf",
+    "url": "/simple-calculator/static/css/main.d78d02b7.chunk.css"
+  },
+  {
+    "revision": "8923da95c5fc16d29007",
+    "url": "/simple-calculator/static/js/2.99703061.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/simple-calculator/static/js/2.99703061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff55a1afe5c7dac5c2cf",
+    "url": "/simple-calculator/static/js/main.0a37d0a7.chunk.js"
+  },
+  {
+    "revision": "b6fca128941aef40cf87",
+    "url": "/simple-calculator/static/js/runtime-main.c1fb96f2.js"
+  }
+]);
